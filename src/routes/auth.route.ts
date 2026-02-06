98f03e8ec625353bb1d9b8393fb92ab47050ec71
yasir5405 <yasirnaseem5415@gmail.com>
2026-02-06 18:13:12 +0530

    Update refresh-token endpoint to post and logout endpoint to post

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -15,8 +15,8 @@ const authRouter = Router();
 authRouter.post("/register", registerUser); //done
 authRouter.post("/login", loginUser); //done
 authRouter.get("/me", verifyJwt, getUserDetails); //done
-authRouter.get("/refresh-token", refreshToken); //done
-authRouter.get("/logout", logout); //done
+authRouter.post("/refresh-token", refreshToken); //done
+authRouter.post("/logout", logout); //done
 authRouter.post("/forgot-password", resetPasswordLink); //done
 authRouter.post("/reset-password", resetPassword); //done
 
